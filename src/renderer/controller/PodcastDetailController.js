4cf03942a3f6e1118da4c61fa47bf0dc7773c15b
Sergio Vilar <sergiovilar.r@gmail.com>
2016-04-11 13:57:39 -0300

    List episodes in Podcast detail view
    This commit also changes some styling for showing Episodes.
    
    Closes #9

--- src/renderer/controller/PodcastDetailController.js
+++ src/renderer/controller/PodcastDetailController.js
@@ -1,7 +1,12 @@
 const Podcast = remote.require('./browser/model/Podcast.js');
+const Episode = remote.require('./browser/model/Episode.js');
 
 app.controller('PodcastDetailController', ['$scope', '$rootScope', '$routeParams', ($scope, $rootScope, $routeParams) => {
 
   $scope.podcast = Podcast.find({id: $routeParams.id})
+  $scope.episodes = Episode
+      .chain()
+      .filter({podcast_id: $routeParams.id})
+      .value();
 
 }]);
