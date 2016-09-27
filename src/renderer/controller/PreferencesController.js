288a98bc58b0a4d39e18b5be48fd529be60450cd
Sergio Vilar <sergiovilar.r@gmail.com>
2016-09-26 21:54:09 -0300

    Add hability to change update channel
    Closes #46

--- src/renderer/controller/PreferencesController.js
+++ src/renderer/controller/PreferencesController.js
@@ -1,2 +1,9 @@
+const ConfigController = remote.require('./browser/controller/ConfigController.js')
+
 app.controller('PreferencesController', ['$scope', ($scope) => {
+  $scope.config = ConfigController._getDefaultDocument()
+
+  $scope.updateChannel = $scope.config.updateChannel === 'beta'
+  $scope.$watch('updateChannel', () => ConfigController.set('updateChannel', $scope.updateChannel ? 'beta' : 'stable'))
+
 }])
