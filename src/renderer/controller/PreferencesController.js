3312b0aee149f8cc381cd363e1966acecdfb9b0c
Sergio Vilar <sergiovilar.r@gmail.com>
2016-09-26 22:00:17 -0300

    Allow users to setup interval do fetch new episodes
    Closes #5

--- src/renderer/controller/PreferencesController.js
+++ src/renderer/controller/PreferencesController.js
@@ -4,6 +4,11 @@ app.controller('PreferencesController', ['$scope', ($scope) => {
   $scope.config = ConfigController._getDefaultDocument()
 
   $scope.updateChannel = $scope.config.updateChannel === 'beta'
+  $scope.fetchEpisodeInterval = $scope.config.fetchEpisodeInterval / 60 / 1000
+
   $scope.$watch('updateChannel', () => ConfigController.set('updateChannel', $scope.updateChannel ? 'beta' : 'stable'))
+  $scope.$watch('fetchEpisodeInterval', () => {
+    ConfigController.set('fetchEpisodeInterval', $scope.fetchEpisodeInterval * 60 * 1000)
+  })
 
 }])
